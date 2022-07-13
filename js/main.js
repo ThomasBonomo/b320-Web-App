90425d285b051c782b8743763f764edcbaf51bf2
ThomasBonomo <54190019+ThomasBonomo@users.noreply.github.com>
2022-07-13 10:56:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,36 @@ function callback(e)
 		}
 		else if (fullJson.sources.length > 0)
 		{
-			
+			if (fullJson.sources[0].startsWith("https://www.twitter"))
+			{
+				resultBody.innerHTML += "<div class=\"result\"><img class=\"result-image\" src=\"" + fullJson.posts[i].sources[0] + "." + fullJson.posts[i].file.ext + "\"><div class=\"result-link\">" + fullJson.posts[i].sources[0] + "." + fullJson.posts[i].file.ext + "</div></div>";
+			}
+			else
+			{
+				resultBody.innerHTML += "<div class=\"result\"><img class=\"result-image\" src=\"" + fullJson.posts[i].sources[0] + "\"><div class=\"result-link\">" + fullJson.posts[i].sources[0] + "</div></div>";
+			}
+		}
+		else
+		{
+			resultBody.innerHTML += "<div class=\"result\">Could not find image.</div>";
 		}
 	}
+}
+
+function srch(cbk)
+{
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function()
+	{
+		if (xhr.readyState == 4 && xhr.status == 200)
+			cbk(xhr.responseText);
+	}
+	var srchText = document.getElementById("sb").value;
+	xhr.open("GET", "https://e621.net/posts.json?limit=24&tags=" + srchText, true);
+	xhr.send();
+}
+
+function search()
+{
+	srch(callback);
 }
